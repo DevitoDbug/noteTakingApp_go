cf63b4868fbb7f40fd391c14686081109d27c2c1
devitoDbug <davidOchiengy@gmail.com>
2023-10-13 22:46:35 +0300

    Define routes

--- pkg/routes/notesRoute.go
+++ pkg/routes/notesRoute.go
@@ -1,1 +1,14 @@
 package routes
+
+import (
+	"github.com/DevitoDbug/noteTakingApp_go/pkg/controllers"
+	"github.com/gorilla/mux"
+)
+
+func RegisterRoutes(r *mux.Router) {
+	r.HandleFunc("/note/", controllers.GetAllNotes).Methods("GET")
+	r.HandleFunc("/note/{id}", controllers.GetNotesById).Methods("GET")
+	r.HandleFunc("/note/", controllers.CreateBook).Methods("POST")
+	r.HandleFunc("/note/{id}", controllers.UpdateBook).Methods("PUT")
+	r.HandleFunc("/note/{id}", controllers.DeleteBook).Methods("Delete")
+}
