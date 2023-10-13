582e55c74dc4a38e605df35225fa3d168fc53716
devitoDbug <davidOchiengy@gmail.com>
2023-10-13 22:51:21 +0300

    Define route handlers

--- pkg/controllers/notesControllers.go
+++ pkg/controllers/notesControllers.go
@@ -1,1 +1,19 @@
 package controllers
+
+import "net/http"
+
+func GetAllNotes(w http.ResponseWriter, r *http.Request) {
+
+}
+func GetNoteById(w http.ResponseWriter, r *http.Request) {
+
+}
+func CreateBook(w http.ResponseWriter, r *http.Request) {
+
+}
+func UpdateBook(w http.ResponseWriter, r *http.Request) {
+
+}
+func DeleteBook(w http.ResponseWriter, r *http.Request) {
+
+}
--- pkg/routes/notesRoute.go
+++ pkg/routes/notesRoute.go
@@ -7,7 +7,7 @@ import (
 
 func RegisterRoutes(r *mux.Router) {
 	r.HandleFunc("/note/", controllers.GetAllNotes).Methods("GET")
-	r.HandleFunc("/note/{id}", controllers.GetNotesById).Methods("GET")
+	r.HandleFunc("/note/{id}", controllers.GetNoteById).Methods("GET")
 	r.HandleFunc("/note/", controllers.CreateBook).Methods("POST")
 	r.HandleFunc("/note/{id}", controllers.UpdateBook).Methods("PUT")
 	r.HandleFunc("/note/{id}", controllers.DeleteBook).Methods("Delete")
