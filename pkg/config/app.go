df9e63cc13b19c524390b83ea61b812cffd2fa0c
devitoDbug <davidOchiengy@gmail.com>
2023-10-21 11:40:55 +0300

    feat: Configure Docker Compose for MySQL Integration

--- pkg/config/app.go
+++ pkg/config/app.go
@@ -4,20 +4,29 @@ import (
 	"fmt"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"time"
 )
 
 var db *gorm.DB
 
 func CreateConnection() error {
-	connectionString := "root:j1751502021@tcp(database:3306)/notesAPI?parseTime=true"
-	d, err := gorm.Open("mysql", connectionString)
-	if err != nil {
-		panic(err)
+	connectionString := "root:j1751502021@tcp(database:3306)/noteAPIDatabase?parseTime=true"
+
+	// Retry the connection until the database is available
+	for {
+		d, err := gorm.Open("mysql", connectionString)
+		if err != nil {
+			fmt.Printf("Error connecting to the database: %v. Retrying...\n", err)
+			time.Sleep(5 * time.Second) // Wait for 5 seconds before retrying
+			continue
+		}
+
+		fmt.Println("Connection made successfully")
+		db = d
+		return nil
 	}
-	fmt.Println("connection made successfully ")
-	db = d
-	return nil
 }
+
 func GetConnection() *gorm.DB {
 	return db
 }
